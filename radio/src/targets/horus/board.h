3a691fb37f60422c6b4ab5e2548cc51247e7e8c9
Goebish <goebish@gmail.com>
2019-04-30 22:56:04 +0200

    Decrease minimum backlight level

--- radio/src/targets/horus/board.h
+++ radio/src/targets/horus/board.h
@@ -486,7 +486,7 @@ void backlightEnable(uint8_t dutyCycle);
 #if defined(PCBX12S)
 #define BACKLIGHT_LEVEL_MIN   5
 #else
-#define BACKLIGHT_LEVEL_MIN   46
+#define BACKLIGHT_LEVEL_MIN   5
 #endif
 #define BACKLIGHT_ENABLE()    backlightEnable(unexpectedShutdown ? BACKLIGHT_LEVEL_MAX : BACKLIGHT_LEVEL_MAX-g_eeGeneral.backlightBright)
 #define BACKLIGHT_DISABLE()   backlightEnable(unexpectedShutdown ? BACKLIGHT_LEVEL_MAX : ((g_eeGeneral.blOffBright == BACKLIGHT_LEVEL_MIN) && (g_eeGeneral.backlightMode != e_backlight_mode_off)) ? 0 : g_eeGeneral.blOffBright)
